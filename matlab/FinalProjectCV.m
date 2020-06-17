fbe5544b23891561d9141cbce6206b21b37cd8e0
DanyOrtu97 <danieleortu8@gmail.com>
2020-06-17 15:21:34 +0200

    Insertion of Project file. Work in progress!

---
+++ matlab/FinalProjectCV.m
@@ -0,0 +1,25 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  Final Project CV 2019                                                  %
+%  Find the position of a person into the store given a classifier ...    %
+%                                                                         %
+%  Students:                                                              %
+%  Daniele Ortu   <danieleortu8@gmail.com>                                %
+%  Andrea Atzori  <atzoriandrea@outlook.it>                               %
+%                                                                         %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+% Acquisition of the test and training set
+% TrainingSetData = ...
+% TestSetData = ...
+
+
+% Convert images of the training set into BoVW representation
+
+
+% Induction of the classifier
+
+
+% Knn to find the closest image to training set from test set (finding the position into the store) 
+
+
